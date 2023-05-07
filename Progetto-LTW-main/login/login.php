736860600dc204834daea3adb75d585ac261b805
fralucente <126571032+fralucente@users.noreply.github.com>
2023-05-07 11:11:18 +0200

    Add files via upload

---
+++ Progetto-LTW-main/login/login.php
@@ -0,0 +1,49 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] != "POST") {
+    header("Location: /");
+}
+else {
+    $dbconn = pg_connect("host=localhost port=5432 dbname=ScimmiApparel
+                user=postgres password=franco,126") 
+                or die('Could not connect: ' . pg_last_error());
+}
+                
+?>
+                
+<!DOCTYPE html>
+<html>
+    <head>
+    <script language="javascript" src="../functions.js" ></script>
+    </head>
+    <body>
+        
+        <?php
+            if ($dbconn) {
+                $email = $_POST['inputEmail'];
+                $q1 = "select * from utente where email= $1";
+                $result = pg_query_params($dbconn, $q1, array($email));
+                $tuple=pg_fetch_array($result, null, PGSQL_ASSOC);
+                if (!($tuple)) {
+                    echo "<h1>Non sembra che ti sia registrato/a</h1>
+                        <a href=../registrazione/index.html> Clicca qui per farlo </a>";
+                }
+                else {
+                    $password = $_POST['inputPassword'];
+                   
+                    if (!(password_verify($password,$tuple["password"]))) {
+                        echo "<h1> La password e' sbagliata! </h1>
+                            <a href=login.html> Clicca qui per loggarti </a>";
+                    }
+                    else {
+                        $nome=$tuple["nome"];
+                        
+                        
+                        
+                       echo "<script>login(".json_encode($email).",".json_encode($nome).");</script>";
+                       
+                    }
+                }
+            }
+        ?> 
+    </body>
+</html>
