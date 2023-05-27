d685c4598c743ba351675dc8ff805e7d72313327
fralucente <126571032+fralucente@users.noreply.github.com>
2023-05-27 18:53:18 +0200

    Add files via upload

---
+++ Progetto-LTW-main/pay_bonus/recupera_bonus.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] != "POST") {
+    header("Location: /");
+}
+else {
+    $dbconn = pg_connect("host=localhost port=5432 dbname=ScimmiApparel
+                user=postgres password=dajeromadaje") 
+                or die('Could not connect: ' . pg_last_error());
+}
+?>
+
+        <?php
+            if ($dbconn) {
+                $email = $_POST['username'];
+                $q1="select bonus from utente where email=$1";
+                $result=pg_query_params($dbconn, $q1, array($email));
+                $row = pg_fetch_array($result, null, PGSQL_ASSOC);
+                $bonus = $row['bonus'];
+                echo $bonus;
+            }
+        ?> 
+   
